ce9a4d1f2975274bda9aec9130e5395b7d45242e
Maxime de Roucy <maxime.deroucy@oxalide.com>
2016-10-31 19:52:40 +0100

    function: remove "hello", add "exist"

--- src/vmod_triggerfile.c
+++ src/vmod_triggerfile.c
@@ -11,77 +11,25 @@
 #include "vtim.h"
 #include "vcc_triggerfile_if.h"
 
-const size_t infosz = 64;
-char	     *info;
-
 /*
  * handle vmod internal state, vmod init/fini and/or varnish callback
  * (un)registration here.
  *
- * malloc'ing the info buffer is only indended as a demonstration, for any
- * real-world vmod, a fixed-sized buffer should be a global variable
+ * this vmod doesn't have any state, so there is nothing to be done
+ *
  */
 
 int __match_proto__(vmod_event_f)
 event_function(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
 {
-	char	   ts[VTIM_FORMAT_SIZE];
-	const char *event = NULL;
-
-	(void) ctx;
-	(void) priv;
-
-	switch (e) {
-	case VCL_EVENT_LOAD:
-		info = malloc(infosz);
-		if (! info)
-			return (-1);
-		event = "loaded";
-		break;
-	case VCL_EVENT_WARM:
-		event = "warmed";
-		break;
-	case VCL_EVENT_COLD:
-		event = "cooled";
-		break;
-	case VCL_EVENT_DISCARD:
-		free(info);
-		return (0);
-		break;
-	default:
-		return (0);
-	}
-	AN(event);
-	VTIM_format(VTIM_real(), ts);
-	snprintf(info, infosz, "vmod_triggerfile %s at %s", event, ts);
-
 	return (0);
 }
 
-VCL_STRING
-vmod_info(VRT_CTX)
+unsigned
+vmod_exist(struct sess *sp, const char *name)
 {
-	(void) ctx;
+	if(access(name, F_OK) == 0)
+		return 1 /* OK */;
 
-	return (info);
-}
-
-VCL_STRING
-vmod_hello(VRT_CTX, VCL_STRING name)
-{
-	char *p;
-	unsigned u, v;
-
-	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
-	p = ctx->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
-	v++;
-	if (v > u) {
-		/* No space, reset and leave */
-		WS_Release(ctx->ws, 0);
-		return (NULL);
-	}
-	/* Update work space with what we've used */
-	WS_Release(ctx->ws, v);
-	return (p);
+	return 0 /* KO */;
 }
